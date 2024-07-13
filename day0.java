45ad0feb48941670980a9dea78fa335869ab1f5e
Malishka Paul <paulmalishka2411@gmail.com>
2024-07-13 23:28:31 +0530

    delete

--- day0.java
+++
@@ -1,6 +0,0 @@
-public class day0 {
-    public static void main(String[] args){
-    System.out.println("HI");
-}
-    
-}
--- homework.java
+++
@@ -1,6 +0,0 @@
-public class homework {
-    public static void main(String[] args){
-    System.out.println("homework for day 1");
-}
-    
-}
